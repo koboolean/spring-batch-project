87b749c64055beb1dc5286eb0f2d01b0f8e650b5
조현준 <45157196+koboolean@users.noreply.github.com>
2024-12-19 21:06:36 +0900

    Spring Batch - Job의 이해

--- src/main/java/com/koboolean/springbatchlecture/config/HelloJobConfig.java
+++ src/main/java/com/koboolean/springbatchlecture/config/HelloJobConfig.java
@@ -1,5 +1,6 @@
 package com.koboolean.springbatchlecture.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.*;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
@@ -9,7 +10,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
+import java.util.Map;
+
 @Configuration
+@RequiredArgsConstructor
 public class HelloJobConfig {
 
     @Bean
@@ -24,9 +28,11 @@ public class HelloJobConfig {
     public Step helloStep(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
         return new StepBuilder("helloStep1", jobRepository)
                 .tasklet((contribution, chunkContext) -> {
-                    System.out.println(" ============================");
-                    System.out.println(" >> Hello Spring Batch");
-                    System.out.println(" ============================");
+
+                    Map<String, Object> jobParameters = chunkContext.getStepContext().getJobParameters();
+
+                    System.out.println(jobParameters);
+
                     return RepeatStatus.FINISHED;
                 }, transactionManager).build();
     }
