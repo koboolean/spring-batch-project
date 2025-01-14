6fcf6d839e1a5f2db1b51ddcc1a7e5b637f3b23f
조현준 <45157196+koboolean@users.noreply.github.com>
2025-01-14 13:18:10 +0900

    spring batch SimpleJob - incrementer

--- src/main/java/com/koboolean/springbatchlecture/config/HelloJobConfig.java
+++ src/main/java/com/koboolean/springbatchlecture/config/HelloJobConfig.java
@@ -6,6 +6,7 @@ import org.springframework.batch.core.*;
 import org.springframework.batch.core.job.DefaultJobParametersValidator;
 import org.springframework.batch.core.job.builder.FlowBuilder;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -14,20 +15,28 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.Flow;
 
 @Configuration
 @RequiredArgsConstructor
 public class HelloJobConfig {
 
+    private final Job job;
+
     @Bean
     public Job helloJob(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
         return new JobBuilder("helloJob", jobRepository)
                 .start(helloStep(jobRepository, transactionManager)) // 처음 시작하는 Step
                 .next(helloStep2(jobRepository, transactionManager)) // 다음 시작하는 Step
-                //.validator(new CustomJobParametersValidator())
-                //.validator(new DefaultJobParametersValidator(new String[]{"name","date"},new String[]{"count"}))
-                .preventRestart()
+                /*.incrementer(parameters -> {
+                    String key = "id";
+                    JobParameters jobParameters = (parameters == null) ? new JobParameters() : parameters;
+
+                    long id = Optional.ofNullable(jobParameters.getLong(key, 0L)).orElse(0L) + 1;
+                    return new JobParametersBuilder(jobParameters).addLong(key, id).toJobParameters();
+                })*/
+                .incrementer(new RunIdIncrementer())
                 .build();
     }
 
