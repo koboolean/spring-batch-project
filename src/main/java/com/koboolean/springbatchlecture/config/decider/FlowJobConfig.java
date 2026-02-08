42e9590c135e5d0d4a383b3b2f84fff87c6b8c47
조현준 <45157196+koboolean@users.noreply.github.com>
2026-02-08 17:39:51 +0900

    commit flow job decider

--- src/main/java/com/koboolean/springbatchlecture/config/decider/FlowJobConfig.java
+++ src/main/java/com/koboolean/springbatchlecture/config/decider/FlowJobConfig.java
@@ -1,12 +1,11 @@
-package com.koboolean.springbatchlecture.config.exitStatus;
+package com.koboolean.springbatchlecture.config.decider;
 
+import com.koboolean.springbatchlecture.config.exitStatus.PassCheckingListener;
 import lombok.RequiredArgsConstructor;
-import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.job.builder.FlowBuilder;
 import org.springframework.batch.core.job.builder.JobBuilder;
-import org.springframework.batch.core.job.flow.Flow;
+import org.springframework.batch.core.job.flow.JobExecutionDecider;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -15,42 +14,40 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @RequiredArgsConstructor
-// @Configuration
+@Configuration
 public class FlowJobConfig {
 
     private final JobRepository jobRepository;
     private final PlatformTransactionManager transactionManager;
 
-    /**
-     * 1. Step1이 실행이 실패한다면 Step2로 가며, Step2가 PASS일 경우 STOP되도록 해보자
-     * 2. Step1이 실행된 이후 PASS가 오지 않는다면 JOB은 실패하게 된다.
-     * 3.
-     * @return
-     */
     @Bean
     public Job flowJob() {
         return new JobBuilder("flowJob", jobRepository)
-                .start(step1())
-                    .on("FAILED")
-                    .to(step2())
-                    .on("PASS") // 사용자 정의 코드로 반환되었다면
-                    .stop()
+                .start(step())
+                .next(decider())
+                .from(decider()).on("EVEN").to(evenStep())
+                .from(decider()).on("ODD").to(oddStep())
                 .end()
                 .build();
     }
 
     @Bean
-    public Step step1() {
+    public JobExecutionDecider decider() {
+        return new OddDecider();
+    }
+
+    @Bean
+    public Step step() {
         return new StepBuilder("step1", jobRepository)
                 .tasklet(((contribution, chunkContext) -> {
                     System.out.println("Step 1 executed!");
-                    contribution.setExitStatus(ExitStatus.FAILED);
+                    //contribution.setExitStatus(ExitStatus.FAILED);
                     return RepeatStatus.FINISHED;
                 }), transactionManager).build();
     }
 
     @Bean
-    public Step step2() {
+    public Step evenStep() {
         return new StepBuilder("step2", jobRepository)
                 .tasklet(((contribution, chunkContext) -> {
                     System.out.println("Step 2 executed!");
@@ -61,7 +58,7 @@ public class FlowJobConfig {
     }
 
     @Bean
-    public Step step3() {
+    public Step oddStep() {
         return new StepBuilder("step3", jobRepository)
                 .tasklet(((contribution, chunkContext) -> {
                     System.out.println("Step 3 executed!");
