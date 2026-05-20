bee7ba69df29e08e7358481f15f8b929109cb18e
koboolean <koboolean@gmail.com>
2026-05-20 13:07:51 +0900

    DelimetedLineTokenizer를 이용한 파일 배치 반영

--- src/main/java/com/koboolean/springbatchlecture/config/flatFileBasic/FlatFileJobRunner.java
+++ src/main/java/com/koboolean/springbatchlecture/config/flatFileBasic/FlatFileJobRunner.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 
-//@Component
+@Component
 public class FlatFileJobRunner implements ApplicationRunner {
 
     private final JobLauncher jobLauncher;
